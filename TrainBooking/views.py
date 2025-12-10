8cf0455072b5834731dd43faafa4b541fecc5bb2
alexlewington1 <s311480@uos.ac.uk>
2025-12-10 11:51:56 +0000

    added html page placeholders and updated views/urls

--- TrainBooking/views.py
+++ TrainBooking/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(response):
+    return render (response, 'index.html')
+def availabletrains(response):
+    return render (response, 'availabletrains.html')
+def createaccount(response):
+    return render (response, 'createaccount.html')
+def login(response):
+    return render (response, 'login.html')
+def ticketbooking(response):
+    return render (response, 'ticketbook.html')
+def ticketsearch(response):
+    return render (response, 'ticketsearch.html')

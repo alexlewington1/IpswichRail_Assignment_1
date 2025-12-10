90bee17009a79b273175278db5a57ade52e05889
alexlewington1 <s311480@uos.ac.uk>
2025-12-10 12:20:43 +0000

    added train times model

--- TrainBooking/models.py
+++ TrainBooking/models.py
@@ -4,6 +4,12 @@ class LoginDetails(models.Model):
     username = models.CharField("Username", max_length=30, unique=True)
     password = models.CharField("Password", max_length=30)
 
+class TrainTimes(models.Model):
+    departure_station = models.CharField("Departure Station", max_length=30)
+    arrival_station = models.CharField("Arrival Station", max_length=30)
+    arrival_time = models.CharField("Arrival Time", max_length=30)
+    departure_time = models.CharField("Departure Time", max_length=30)
+    price = models.DecimalField("Price", max_digits=10, decimal_places=2)
 
 
 

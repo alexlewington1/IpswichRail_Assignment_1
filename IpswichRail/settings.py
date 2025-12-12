687e4d8465a0f618116b553fd3677dbef652ac7c
alexlewington1 <s311480@uos.ac.uk>
2025-12-12 20:47:41 +0000

    uploaded allowed host for render deployment

--- IpswichRail/settings.py
+++ IpswichRail/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2kiry6$0irk=jz5))^ru1b3tggjy5&1rd*ayn4kfu8k7lm6@h$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
